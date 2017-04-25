290c17245bda2b7f8a8bdb6513344154f0d449c8
nguyen,bao <bao.nguyen@ingramcontent.com>
2017-04-25 18:03:18 -0500

    coding

--- javascripts/main.js
+++ javascripts/main.js
@@ -1,1 +1,25 @@
-console.log("Javascript loading");
+$(document).ready(() => {
+	const apiKey = "";
+
+	$("body").on("click", "li", (e) => {
+		loadAPIRequest(e.target.innerHTML).then((data) => {
+			let info = data.results;
+			for (let i = 0; i < info.length; i++){
+				console.log(info[i].name);
+			}
+		}).catch((error) => {
+			console.log(error);
+		});
+	});
+
+	const loadAPIRequest = (dropdownType) => {
+		return new Promise ((resolve, reject) => {
+			$.ajax(`https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=36.174465,-86.767960&radius=50000&type=${dropdownType}&key=${apiKey}`).done((data) => resolve(data))
+				.fail((error) => reject(error));
+		});
+	};
+
+
+
+
+});
